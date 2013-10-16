e563da4704790790dd4bd13232af462273aba37a
zcarioca <rchargel@zcarioca.net>
2013-10-16 15:28:15 -0400

    Updating country validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,6 @@
 class Country < ActiveRecord::Base
+   validates :name, :presence => true, :uniqueness => true, :length { :maximum => 100 }
+   validates :officialName, :length { :maximum => 255}
+   validates :iso2Code, :length { :is => 2 }
+   validates :iso3Code, :length { :is => 3 }
 end
