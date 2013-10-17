2e7bb4d7035a74dccb776bbb1302b1dd7afeb6e7
zcarioca <rchargel@zcarioca.net>
2013-10-16 20:06:15 -0400

    Recreating repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CslLookup::Application.config.secret_key_base = '16ec898de34520bd81772a867b79cb356a193c7796bf7f629a223e54c2184fd0c41d1bb684fea90461a304082c3b702860da597669f824ad20a8085bd7a2ae93'
